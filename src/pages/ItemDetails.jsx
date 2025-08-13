b25423c6a52ac5b42dad1c32b0f176718b01e3a5
deepakkolli0 <rambutanandlycheejuice@gmail.com>
2025-08-13 13:10:19 -0400

    Added dynamic loading for the NFTs via an API, with dynamic routing and a carousel for a clean display

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,13 +1,211 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import EthImage from "../images/ethereum.svg";
 import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
 
 const ItemDetails = () => {
+  const { nftId } = useParams();
+  const [nftData, setNftData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+
+    const fetchNftData = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+        );
+        const nft = response.data.find(
+          (item) => item.nftId.toString() === nftId
+        );
+
+        if (nft) {
+          setNftData(nft);
+        } else {
+          console.log("NFT not found");
+        }
+      } catch (error) {
+        console.error("Error fetching NFT data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNftData();
+  }, [nftId]);
+
+  if (loading) {
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <div id="top"></div>
+          <section aria-label="section" className="mt90 sm-mt-0">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6 text-center">
+                  <div
+                    className="skeleton-image"
+                    style={{
+                      width: "100%",
+                      height: "400px",
+                      backgroundColor: "#e0e0e0",
+                      borderRadius: "10px",
+                      animation: "pulse 1.5s ease-in-out infinite",
+                    }}
+                  ></div>
+                </div>
+                <div className="col-md-6">
+                  <div className="item_info">
+                    <div
+                      className="skeleton-title"
+                      style={{
+                        width: "70%",
+                        height: "32px",
+                        backgroundColor: "#e0e0e0",
+                        borderRadius: "4px",
+                        marginBottom: "20px",
+                        animation: "pulse 1.5s ease-in-out infinite",
+                      }}
+                    ></div>
+
+                    <div className="item_info_counts">
+                      <div className="item_info_views">
+                        <i className="fa fa-eye"></i>
+                        <div
+                          className="skeleton-text"
+                          style={{
+                            width: "30px",
+                            height: "16px",
+                            backgroundColor: "#e0e0e0",
+                            borderRadius: "4px",
+                            display: "inline-block",
+                            marginLeft: "8px",
+                            animation: "pulse 1.5s ease-in-out infinite",
+                          }}
+                        ></div>
+                      </div>
+                      <div className="item_info_like">
+                        <i className="fa fa-heart"></i>
+                        <div
+                          className="skeleton-text"
+                          style={{
+                            width: "30px",
+                            height: "16px",
+                            backgroundColor: "#e0e0e0",
+                            borderRadius: "4px",
+                            display: "inline-block",
+                            marginLeft: "8px",
+                            animation: "pulse 1.5s ease-in-out infinite",
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+
+                    <div
+                      className="skeleton-description"
+                      style={{
+                        width: "100%",
+                        height: "60px",
+                        backgroundColor: "#e0e0e0",
+                        borderRadius: "4px",
+                        marginBottom: "20px",
+                        animation: "pulse 1.5s ease-in-out infinite",
+                      }}
+                    ></div>
+
+                    <div className="item__details--flex-row">
+                      <div className="item__details--margin-right">
+                        <h6>Owner</h6>
+                        <div className="item__details--author">
+                          <div className="item__details--author__avatar">
+                            <div
+                              className="skeleton__avatar"
+                              style={{
+                                width: "50px",
+                                height: "50px",
+                                backgroundColor: "#e0e0e0",
+                                borderRadius: "50%",
+                                animation: "pulse 1.5s ease-in-out infinite",
+                              }}
+                            ></div>
+                          </div>
+                          <div className="item__details--author__info">
+                            <div
+                              className="skeleton__text"
+                              style={{
+                                width: "100px",
+                                height: "16px",
+                                backgroundColor: "#e0e0e0",
+                                borderRadius: "4px",
+                                animation: "pulse 1.5s ease-in-out infinite",
+                              }}
+                            ></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="item__details--tab tab_simple">
+                      <div className="item__details--tab__content">
+                        <h6>Creator</h6>
+                        <div className="item__details--author">
+                          <div className="item__details--author__avatar">
+                            <div
+                              className="skeleton__avatar"
+                              style={{
+                                width: "50px",
+                                height: "50px",
+                                backgroundColor: "#e0e0e0",
+                                borderRadius: "50%",
+                                animation: "pulse 1.5s ease-in-out infinite",
+                              }}
+                            ></div>
+                          </div>
+                          <div className="item__details--author__info">
+                            <div
+                              className="skeleton__text"
+                              style={{
+                                width: "100px",
+                                height: "16px",
+                                backgroundColor: "#e0e0e0",
+                                borderRadius: "4px",
+                                animation: "pulse 1.5s ease-in-out infinite",
+                              }}
+                            ></div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="item__details--spacer"></div>
+                      <h6>Price</h6>
+                      <div className="item__details--price">
+                        <img src={EthImage} alt="" />
+                        <div
+                          className="skeleton__text"
+                          style={{
+                            width: "60px",
+                            height: "20px",
+                            backgroundColor: "#e0e0e0",
+                            borderRadius: "4px",
+                            display: "inline-block",
+                            marginLeft: "8px",
+                            animation: "pulse 1.5s ease-in-out infinite",
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div id="wrapper">
@@ -18,14 +216,16 @@ const ItemDetails = () => {
             <div className="row">
               <div className="col-md-6 text-center">
                 <img
-                  src={nftImage}
+                  src={nftData.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
+                  alt={nftData.title}
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>
+                    {nftData.title} #{nftData.nftId}
+                  </h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
@@ -38,47 +238,55 @@ const ItemDetails = () => {
                     </div>
                   </div>
                   <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
+                    This is a unique NFT from the {nftData.title} collection.
+                    Each piece in this collection is carefully crafted and
+                    represents a unique digital asset on the blockchain.
                   </p>
-                  <div className="d-flex flex-row">
-                    <div className="mr40">
+                  <div className="item__details--flex-row">
+                    <div className="item__details--margin-right">
                       <h6>Owner</h6>
-                      <div className="item_author">
-                        <div className="author_list_pp">
+                      <div className="item__details--author">
+                        <div className="item__details--author__avatar">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                            <img
+                              className="lazy"
+                              src={nftData.authorImage}
+                              alt=""
+                            />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
-                        <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                        <div className="item__details--author__info">
+                          <Link to="/author">NFT Owner</Link>
                         </div>
                       </div>
                     </div>
                     <div></div>
                   </div>
-                  <div className="de_tab tab_simple">
-                    <div className="de_tab_content">
+                  <div className="item__details--tab tab_simple">
+                    <div className="item__details--tab__content">
                       <h6>Creator</h6>
-                      <div className="item_author">
-                        <div className="author_list_pp">
+                      <div className="item__details--author">
+                        <div className="item__details--author__avatar">
                           <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                            <img
+                              className="lazy"
+                              src={nftData.authorImage}
+                              alt=""
+                            />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
-                        <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                        <div className="item__details--author__info">
+                          <Link to="/author">NFT Creator</Link>
                         </div>
                       </div>
                     </div>
-                    <div className="spacer-40"></div>
+                    <div className="item__details--spacer"></div>
                     <h6>Price</h6>
-                    <div className="nft-item-price">
+                    <div className="item__details--price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>1.00</span>
                     </div>
                   </div>
                 </div>
